96371a50f76627032f1df599e9fb3e1b8d666402
Horfee <horfee@gmail.com>
2023-02-07 12:43:02 +0100

    adding script file for building images

---
+++ deployapp/buildImages.sh
@@ -0,0 +1,12 @@
+rm -rf ./dist
+cp -r ../dist ./
+
+version=$(jq '.version' ../package.json)
+if [[ "$version" = "" ]]; then
+    version="latest"
+fi
+
+docker context use colima-profile_x86_64
+docker run --privileged --rm docker binfmt:a7996909642ee92942dcd6cff44b9b95f08dad64
+docker buildx create --use
+docker buildx build --platform linux/arm64,linux/arm/v7,linux/arm/v5,linux/amd64,linux/386,linux/arm/v8 -t horfee/quicknote:$version -t horfee/quicknote:latest . --push
